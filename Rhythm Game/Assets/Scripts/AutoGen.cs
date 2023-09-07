8293e4d4ddb5755e52a26e43a4b46ecbf5e70216
Gino <35615@ma-web.nl>
2023-09-07 14:18:35 +0200

    lvl change

---
+++ Rhythm Game/Assets/Scripts/AutoGen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+
+namespace Assets.Scripts
+{
+    internal class AutoGen
+    {
+        public float _barTime;
+        public float _noteTime;
+        public float _quantize;
+        public float _bpm;
+
+        private void Calculate()
+        {
+            _barTime = (60 / (_bpm / 4) * 1000);
+            _noteTime = _barTime / _quantize;
+        }
+
+        private void Update()
+        {
+            _timer.Update();
+
+            
+        }
+
+    }
+}
